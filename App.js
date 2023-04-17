73dc05aef4d31a9f9c16dea97be47eded931ad08
abdulumar1111 <abdulumar1111@gmail.com>
2023-04-17 23:34:43 +0530

    react dark code

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import {View,Text} from 'react-native';
+import {View, Text, useColorScheme, StyleSheet} from 'react-native';
 
 const App = () => {
-  return(
-    <View>
-      <Text>Hello Zeehsan Ansari</Text>
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        Hello Zeehsan Ansari
+      </Text>
     </View>
-  )
-}
+  );
+};
 
- export default App;
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: '#fff',
+  },
+  darkText: {
+    color: '#000',
+  },
+});
